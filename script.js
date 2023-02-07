4a803538b8a2a7cc236c49da9da156b91e79aa28
sameedmohsin28 <sameedmohsin22@hotmail.com>
2023-02-08 03:31:35 +0500

    Add for loop to apply content

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const cardWorks = [
     'developerRole': 'Back End Dev',
     'projectYear': '2015',
     'projectDescription': 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    'technologies': ['html', 'css', 'javascript'],
+    'technologies': ['html', 'css', 'javascript', 'Ruby on rails'],
   },
 
   {
@@ -35,7 +35,7 @@ const cardWorks = [
     'developerRole': 'Full Stack Dev',
     'projectYear': '2015',
     'projectDescription': 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
-    'technologies': ['html', 'css', 'javascript'],
+    'technologies': ['html', 'css', 'javascript', 'Ruby on rails'],
   },
 
   {
@@ -45,17 +45,17 @@ const cardWorks = [
     'developerRole': 'Full Stack Dev',
     'projectYear': '2015',
     'projectDescription': 'Exploring the future of media in Facebook&#8216;s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
-    'technologies': ['html', 'Ruby on rails', 'css', 'javascript'],
+    'technologies': ['html', 'css', 'javascript', 'Ruby on rails'],
   },
 
   {
-    'projectImage': './Images/4 multi-post stories.png',
+    'projectImage': './Images/4cardImage.png',
     'projectName': 'Uber Navigation',
     'clientName': 'uber',
     'developerRole': 'Full Stack Dev',
     'projectYear': '2018',
     'projectDescription': 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
-    'technologies': ['html', 'Ruby on rails', 'css', 'javascript'],
+    'technologies': ['html', 'css', 'javascript', 'Ruby on rails'], //Ruby on rails is not required here. The first three ruby are applied to the last three cardworks.
   }
 ]
 
@@ -73,6 +73,7 @@ let html = document.querySelectorAll('.html');
 let css = document.querySelectorAll('.css');
 let javascript = document.querySelectorAll('.javascript');
 let rubyOnRails = document.querySelectorAll('.ruby-on-rails');
+let projectImage = document.querySelectorAll('.work-project-image')
 
 for (let i = 0; i < 4; i++) {
   Array.from(mobileHeading)[i].innerHTML = cardWorks[i]['projectName'];
@@ -81,6 +82,16 @@ for (let i = 0; i < 4; i++) {
   Array.from(desktopClientName)[i].innerHTML = cardWorks[i]['clientName'];
   Array.from(mobileDeveloperRole)[i].innerHTML = cardWorks[i]['developerRole'];
   Array.from(desktopDeveloperRole)[i].innerHTML = cardWorks[i]['developerRole'];
+  Array.from(mobileProjectYear)[i].innerHTML = cardWorks[i]['projectYear'];
+  Array.from(desktopProjectYear)[i].innerHTML = cardWorks[i]['projectYear'];
+  Array.from(mobileProjectDescription)[i].innerHTML = cardWorks[i]['projectDescription'];
+  Array.from(desktopProjectDescription)[i].innerHTML = cardWorks[i]['projectDescription'];
+  Array.from(html)[i].innerHTML = cardWorks[i]['technologies'][0]
+  Array.from(css)[i].innerHTML = cardWorks[i]['technologies'][1]
+  Array.from(javascript)[i].innerHTML = cardWorks[i]['technologies'][2]
+  Array.from(rubyOnRails)[i].innerHTML = cardWorks[i]['technologies'][3]
+  Array.from(projectImage)[i].src = cardWorks[i]['projectImage']
 }
+
 
 
