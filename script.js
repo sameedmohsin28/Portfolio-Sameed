fcaa927b034a3b18091ed610d81d8d468fc742d1
sameedmohsin28 <sameedmohsin22@hotmail.com>
2023-02-07 19:31:10 +0500

    Add backdrop-filter to css

--- script.js
+++ script.js
@@ -2,4 +2,3 @@ let btn = document.querySelector(".hamburger-btn");
 let mobileMenu = document.querySelector(".nav-list-mobile-container");
 let closeMenu = document.querySelector(".nav-cross-icon")
 let mobileNavLinks = document.querySelector(".nav-link-mobile")
-
