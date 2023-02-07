3b28f2103a7e4e392a171233202b501eeca723b1
sameedmohsin28 <sameedmohsin22@hotmail.com>
2023-02-07 17:17:09 +0500

    Design menu with JS

---
+++ script.js
@@ -0,0 +1,5 @@
+let btn = document.querySelector(".hamburger-btn");
+let mobileMenu = document.querySelector(".nav-list-mobile-container");
+let closeMenu = document.querySelector(".nav-cross-icon")
+let mobileNavLinks = document.querySelector(".nav-link-mobile")
+
