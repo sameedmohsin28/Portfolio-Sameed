1f6f00cb405518c30d04e6aa256648c7ca45ba15
tawakuliKH <tawakuli456@gmail.com>
2023-02-07 19:11:54 +0430

    Add function toggleMenu in script.js file

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ let btn = document.querySelector(".hamburger-btn");
 let mobileMenu = document.querySelector(".nav-list-mobile-container");
 let closeMenu = document.querySelector(".nav-cross-icon")
 let mobileNavLinks = document.querySelector(".nav-link-mobile")
+function toggleMenu () {
+  if (mobileMenu.style.display == "none") {
+      mobileMenu.style.display = "block";
+  } else {
+      mobileMenu.style.display = 'none'; 
+  }
+}
+
+btn.addEventListener('click', toggleMenu);
+closeMenu.addEventListener('click', toggleMenu);
+mobileNavLinks.addEventListener('click', toggleMenu);
