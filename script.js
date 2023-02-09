4f5eadcf510cce8b1c8d109e1b1030122cb8a12b
sameedmohsin28 <sameedmohsin22@hotmail.com>
2023-02-09 19:01:25 +0500

    Create popup section

--- script.js
+++ script.js
@@ -25,7 +25,8 @@ const cardWorks = [
     'developerRole': 'Back End Dev',
     'projectYear': '2015',
     'projectDescription': 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    'technologies': ['html', 'css', 'javascript', 'Ruby on rails'],
+    'projectDescPopupDesk': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#8216;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#8216;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#8216;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    'technologies': ['html', 'css', 'javascript', 'Ruby on rails', 'github', 'Bootstraps', 'ruby'],
   },
 
   {
@@ -35,7 +36,8 @@ const cardWorks = [
     'developerRole': 'Full Stack Dev',
     'projectYear': '2015',
     'projectDescription': 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
-    'technologies': ['html', 'css', 'javascript', 'Ruby on rails'],
+    'projectDescPopupDesk': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#8216;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#8216;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#8216;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    'technologies': ['html', 'css', 'javascript', 'Ruby on rails', 'github', 'Bootstraps', 'ruby'],
   },
 
   {
@@ -45,53 +47,106 @@ const cardWorks = [
     'developerRole': 'Full Stack Dev',
     'projectYear': '2015',
     'projectDescription': 'Exploring the future of media in Facebook&#8216;s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
-    'technologies': ['html', 'css', 'javascript', 'Ruby on rails'],
+    'projectDescPopupDesk': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#8216;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#8216;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#8216;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    'technologies': ['html', 'css', 'javascript', 'Ruby on rails', 'github', 'Bootstraps', 'ruby'],
   },
 
   {
     'projectImage': './Images/4cardImage.png',
     'projectName': 'Uber Navigation',
-    'clientName': 'uber',
+    'clientName': 'Uber',
     'developerRole': 'Full Stack Dev',
     'projectYear': '2018',
     'projectDescription': 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
-    'technologies': ['html', 'css', 'javascript', 'Ruby on rails'], //Ruby on rails is not required here. The first three ruby are applied to the last three cardworks.
+    'projectDescPopupDesk': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#8216;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#8216;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#8216;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    'technologies': ['html', 'css', 'javascript', 'Ruby on rails', 'github', 'Bootstraps', 'ruby'],
   }
 ]
 
-let mobileHeading = document.querySelectorAll('.work-project-details-heading');
-let desktopHeading = document.querySelectorAll('.desktop-work-project-details-heading');
-let mobileClientName = document.querySelectorAll('.client-name');
-let desktopClientName = document.querySelectorAll('.desktop-client-name');
-let mobileDeveloperRole = document.querySelectorAll('.your-role');
-let desktopDeveloperRole = document.querySelectorAll('.desktop-your-role');
-let mobileProjectYear = document.querySelectorAll('.project-year');
-let desktopProjectYear = document.querySelectorAll('.desktop-project-year');
-let mobileProjectDescription = document.querySelectorAll('.work-project-description');
-let desktopProjectDescription = document.querySelectorAll('.desktop-work-project-description');
-let html = document.querySelectorAll('.html');
-let css = document.querySelectorAll('.css');
-let javascript = document.querySelectorAll('.javascript');
-let rubyOnRails = document.querySelectorAll('.ruby-on-rails');
-let projectImage = document.querySelectorAll('.work-project-image');
+const worksPage = document.querySelector('.works-page');
 
-for (let i = 0; i < 4; i++) {
-  Array.from(mobileHeading)[i].innerHTML = cardWorks[i]['projectName'];
-  Array.from(desktopHeading)[i].innerHTML = cardWorks[i]['projectName'];
-  Array.from(mobileClientName)[i].innerHTML = cardWorks[i]['clientName'];
-  Array.from(desktopClientName)[i].innerHTML = cardWorks[i]['clientName'];
-  Array.from(mobileDeveloperRole)[i].innerHTML = cardWorks[i]['developerRole'];
-  Array.from(desktopDeveloperRole)[i].innerHTML = cardWorks[i]['developerRole'];
-  Array.from(mobileProjectYear)[i].innerHTML = cardWorks[i]['projectYear'];
-  Array.from(desktopProjectYear)[i].innerHTML = cardWorks[i]['projectYear'];
-  Array.from(mobileProjectDescription)[i].innerHTML = cardWorks[i]['projectDescription'];
-  Array.from(desktopProjectDescription)[i].innerHTML = cardWorks[i]['projectDescription'];
-  Array.from(projectImage)[i].src = cardWorks[i]['projectImage'];
-  Array.from(html)[i].innerHTML = cardWorks[i]['technologies'][0];
-  Array.from(css)[i].innerHTML = cardWorks[i]['technologies'][1];
-  Array.from(javascript)[i].innerHTML = cardWorks[i]['technologies'][2];
-  Array.from(rubyOnRails)[i].innerHTML = cardWorks[i]['technologies'][3];
+cardWorks.forEach((project) => {
+  worksPage.innerHTML += `
+  <article class="project-work-card">
+    <img src="${project.projectImage}" alt="Screenshot of first project" class="work-project-image">
+    <div class="work-project-details">
+      <h2 class="work-project-details-heading">${project.projectName}</h2>
+      <div class="work-project-highlights">
+        <span class="client-name">${project.clientName}</span>
+        <img src="./Images/Counter.png" alt="" class="dot">
+        <span class="your-role">${project.developerRole}</span>
+        <img src="./Images/Counter.png" alt="" class="dot">
+        <span class="project-year">${project.projectYear}</span>
+      </div>
+      <p class="work-project-description">${project.projectDescription}</p>
+      <ul class="project-languages-tags">
+        <li class="html">${project.technologies[0]}</li>
+        <li class="ruby-on-rails">${project.technologies[3]}</li>
+        <li class="css">${project.technologies[1]}</li>
+        <li class="javascript">${project.technologies[2]}</li>
+      </ul>
+      <button class="project-link-button">See Project</button>
+    </div>
+  </article>
+  `;
+});
+
+const popupWorks = document.querySelector('.popup-works');
+
+cardWorks.forEach((project) => {
+  popupWorks.innerHTML += `
+  <article class="popup-card">
+    <div class="popup-work-project-details">
+      <div class="popup-heading-close-button">
+        <h2 class="popup-work-project-details-heading">${project.projectName}</h2>
+        <img src="./Images/popupCross.png" alt="" class="popup-cross-button">
+      </div>
+      <div class="popup-work-project-highlights">
+        <span class="popup-client-name">${project.clientName}</span>
+        <img src="./Images/Counter.png" alt="" class="dot">
+        <span class="popup-your-role">${project.developerRole}</span>
+        <img src="./Images/Counter.png" alt="" class="dot">
+        <span class="popup-project-year">${project.projectYear}</span>
+      </div>
+    </div>
+    <img src="${project.projectImage}" alt="Screenshot of first project" class="popup-work-project-image">
+    <div class="popup-description-tags-buttons">
+      <p class="popup-work-project-description">${project.projectDescPopupDesk}</p>
+      <div class="popup-tags-buttons">
+        <ul class="popup-first-three-tags">
+          <li class="popup-technology popup-html">${project.technologies[0]}</li>
+          <li class="popup-technology popup-css">${project.technologies[1]}</li>
+          <li class="popup-technology popup-javascript">${project.technologies[2]}</li>
+        </ul>
+        <ul class="popup-last-three-tags">
+          <li class="popup-technology popup-github">${project.technologies[4]}</li>
+          <li class="popup-technology popup-ruby">${project.technologies[6]}</li>
+          <li class="popup-technology   popup-bootstraps">${project.technologies[5]}</li>
+        </ul>
+        <img src="./Images/Devider.png" alt="" class="popup-tags-buttons-divider">
+        <div class="popup-buttons">
+          <button class="popup-see-live-button">See live <img src="./Images/seeLiveButton.png" alt="" class="see-live-icon"></button>
+          <button class="popup-see-source-button">See source <img src="./Images/seeSourceButon.svg" alt="" class="see-source-icon"></button>
+        </div>
+      </div>
+    </div>
+  </article>
+  `;
+});
+
+const seeProject = document.querySelector('.project-link-button');
+const closePopup = document.querySelector('.popup-cross-button');
+const popupCard = document.querySelector('.popup-card');
+
+function togglePopup () {
+  if (popupWorks.style.display === none  && popupCard.style.display === none) {
+    popupWorks.style.display === block;
+    popupCard.style.display === flex;
+  } else {
+    popupWorks.style.display === none;
+    popupCard.style.display === none;
+  }
 }
 
-
-
+seeProject.addEventListener('click', togglePopup);
+closePopup.addEventListener('click', togglePopup);
