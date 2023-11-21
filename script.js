397a350b53c528649815293f7d636d22b5956789
sameedmohsin28 <sameedmohsin22@hotmail.com>
2023-11-22 00:18:35 +0500

    Update portfolio with actual data

--- script.js
+++ script.js
@@ -19,47 +19,55 @@ mobileNavLinks.forEach((menuItem) => {
 
 const cardWorks = [
   {
-    projectImage: './Images/1 tonic.png',
-    projectName: 'Tonic',
-    clientName: 'CANOPY',
-    developerRole: 'Back End Dev',
-    projectYear: '2015',
-    projectDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    projectDescPopupDesk: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#8216;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#8216;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#8216;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-    technologies: ['html', 'css', 'javascript', 'Ruby on rails', 'github', 'Bootstraps', 'ruby'],
+    projectImage: './Images/fastLaneRacers.png',
+    projectName: 'Fast Lane Racers',
+    clientName: 'Personal Project',
+    developerRole: 'Full-stack',
+    projectYear: '2023',
+    projectDescription: 'An app for users to book an appointment to test drive cars.',
+    projectDescPopupDesk: 'An app for users to book an appointment to test drive cars.',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Ruby on rails', 'PostgreSQL'],
+    sourceCodeLink: 'https://github.com/muneeb3123/Fast-Lane-Racers-front-end',
+    liveDeployedLink: 'https://shiny-longma-bba6fb.netlify.app/',
   },
 
   {
-    projectImage: './Images/2 multi-post stories.png',
-    projectName: 'Multi-Post Stories',
-    clientName: 'FACEBOOK',
-    developerRole: 'Full Stack Dev',
-    projectYear: '2015',
-    projectDescription: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
-    projectDescPopupDesk: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#8216;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#8216;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#8216;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-    technologies: ['html', 'css', 'javascript', 'Ruby on rails', 'github', 'Bootstraps', 'ruby'],
+    projectImage: './Images/expenseTracker.png',
+    projectName: 'Expense Tracker',
+    clientName: 'Personal Project',
+    developerRole: 'Full-stack',
+    projectYear: '2023',
+    projectDescription: 'This app lets users keep track of their expenses and divide them into categories..',
+    projectDescPopupDesk: 'This app lets users keep track of their expenses and divide them into categories.',
+    technologies: ['Ruby on rails', 'PostgreSQL'],
+    sourceCodeLink: 'https://github.com/sameedmohsin28/Expense-Tracker',
+    liveDeployedLink: 'https://rails-xez4.onrender.com/',
   },
 
   {
-    projectImage: './Images/3 tonic.png',
-    projectName: 'Facebook 360',
-    clientName: 'FACEBOOK',
-    developerRole: 'Full Stack Dev',
-    projectYear: '2015',
-    projectDescription: 'Exploring the future of media in Facebook&#8216;s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
-    projectDescPopupDesk: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#8216;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#8216;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#8216;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea <br>',
-    technologies: ['html', 'css', 'javascript', 'Ruby on rails', 'github', 'Bootstraps', 'ruby'],
+    projectImage: './Images/forexRates.png',
+    projectName: 'ForEx Rates',
+    clientName: 'Personal Project',
+    developerRole: 'Front-end',
+    projectYear: '2023',
+    projectDescription: 'Fetches data from exchange rate API to display currency exchange rates and lets users convert any specific amount from one currency to another.',
+    projectDescPopupDesk: 'Fetches data from exchange rate API to display currency exchange rates and lets users convert any specific amount from one currency to another.',
+    technologies: ['HTMLS', 'CSS', 'JavaScript', 'React', 'Redux'],
+    sourceCodeLink: 'https://github.com/sameedmohsin28/currency-exchange-rate',
+    liveDeployedLink: 'https://forexrates.netlify.app/',
   },
 
   {
-    projectImage: './Images/4cardImage.png',
-    projectName: 'Uber Navigation',
-    clientName: 'Uber',
-    developerRole: 'Full Stack Dev',
-    projectYear: '2018',
-    projectDescription: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
-    projectDescPopupDesk: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#8216;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#8216;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#8216;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-    technologies: ['html', 'css', 'javascript', 'Ruby on rails', 'github', 'Bootstraps', 'ruby'],
+    projectImage: './Images/restaurantMeal.png',
+    projectName: 'Restaurant Meals',
+    clientName: 'Personal Project',
+    developerRole: 'Front-end',
+    projectYear: '2023',
+    projectDescription: 'Fetches data from MealDB API to display meals and their recipes. It also lets users comment and book reservations on those meals.',
+    projectDescPopupDesk: 'Fetches data from MealDB API to display meals and their recipes. It also lets users comment and book reservations on those meals.',
+    technologies: ['HTMLS', 'CSS', 'JavaScript', 'React', 'Redux'],
+    sourceCodeLink: 'https://github.com/sameedmohsin28/space-traveller-s-hub',
+    liveDeployedLink: 'http://clintonyounge.me/JS-Capstone-Group-Project/dist/',
   },
 ];
 
@@ -80,10 +88,9 @@ cardWorks.forEach((project) => {
       </div>
       <p class="work-project-description">${project.projectDescription}</p>
       <ul class="project-languages-tags">
-        <li class="html">${project.technologies[0]}</li>
-        <li class="ruby-on-rails">${project.technologies[3]}</li>
-        <li class="css">${project.technologies[1]}</li>
-        <li class="javascript">${project.technologies[2]}</li>
+        ${project.technologies.map((tech) => 
+          `<li class="tech">${tech}</li>`
+        ).join('')}
       </ul>
       <button class="project-link-button">See Project</button>
     </div>
@@ -113,20 +120,15 @@ cardWorks.forEach((project) => {
     <div class="popup-description-tags-buttons">
       <p class="popup-work-project-description">${project.projectDescPopupDesk}</p>
       <div class="popup-tags-buttons">
-        <ul class="popup-first-three-tags">
-          <li class="popup-technology popup-html">${project.technologies[0]}</li>
-          <li class="popup-technology popup-css">${project.technologies[1]}</li>
-          <li class="popup-technology popup-javascript">${project.technologies[2]}</li>
-        </ul>
-        <ul class="popup-last-three-tags">
-          <li class="popup-technology popup-github">${project.technologies[4]}</li>
-          <li class="popup-technology popup-ruby">${project.technologies[6]}</li>
-          <li class="popup-technology   popup-bootstraps">${project.technologies[5]}</li>
+        <ul class="popup-tags">
+        ${project.technologies.map((tech) => 
+          `<li class="tech">${tech}</li>`
+        ).join('')}
         </ul>
         <img src="./Images/Devider.png" alt="" class="popup-tags-buttons-divider">
         <div class="popup-buttons">
-          <button class="popup-see-live-button">See live <img src="./Images/seeLiveButton.png" alt="" class="see-live-icon"></button>
-          <button class="popup-see-source-button">See source <img src="./Images/seeSourceButon.svg" alt="" class="see-source-icon"></button>
+          <a href="${project.liveDeployedLink}" class="popup-see-live-button" target="_blank">See live <img src="./Images/seeLiveButton.png" alt="" class="see-live-icon"></a>
+          <a href="${project.sourceCodeLink}" class="popup-see-source-button" target="_blank">See source <img src="./Images/seeSourceButon.svg" alt="" class="see-source-icon"></a>
         </div>
       </div>
     </div>
@@ -140,7 +142,7 @@ const popupCard = Array.from(document.querySelectorAll('.popup-card'));
 
 for (let i = 0; i < cardWorks.length; i += 1) {
   seeProject[i].addEventListener('click', () => {
-    popupCard[i].style.display = 'flex';
+    popupCard[i].style.display = 'grid';
   });
 
   closePopup[i].addEventListener('click', () => {
